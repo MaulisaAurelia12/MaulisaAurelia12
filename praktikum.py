969925087cd397c08bff71c0bc62a3f38e393536
MaulisaAurelia12 <161400281+MaulisaAurelia12@users.noreply.github.com>
2024-09-29 12:04:37 +0700

    Add files via upload

---
+++ praktikum.py
@@ -0,0 +1,30 @@
+### LAPORAN PRAKTIKUM INFRASTRUKTUR DAN PLATFORM UNTUK SAINS DATA ###
+
+## Dasar Teori
+# 1. Variabel dan Tipe Data
+'''
+variabel adalah tempat menyimpan data dalam memori dengan aturan penamaannya seperti menggunakan huruf, angka, dan underscore serta tidak boleh diawali dengan angka.
+'''
+
+x = 13
+y = 157.6
+program_studi = 'Sains Data'
+
+'''
+terdapat beberapa tipe data di Python yang dapat diolah, seperti:
+a. integer (bilangan bulat)
+b. float (bilangan desimal atau pecahan)
+c. string (rangkaian karakter (char))
+d. boolean (didefinisikan dengan nilai True (1) atau False (0)
+'''
+
+umur = 19 # int
+print(type(umur))
+tinggi = 1.57 #float
+print(type(tinggi))
+nama = "lisa" #string
+print(type(nama))
+worker = False #boolean
+print(type(worker))
+
+# 2. Operator dan Logika
